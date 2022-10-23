0474d0691227ef63c783d2124758cf41a593652d
MDhanesh <mgdhanesh98@gmail.com>
2022-10-23 22:09:47 +0530

    c

--- src/component/forgot.js
+++ src/component/forgot.js
@@ -3,54 +3,6 @@ import { Typography, TextField, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-// export default function Forgot() {
-//   const navigate = useNavigate();
-//   const [email, setEmail] = useState("");
-//   const handlesubmit = async (e) => {
-//     e.preventDefault();
-//     const users = {
-//       email: email,
-//     };
-//     const response = await axios.post(
-//       "http://localhost:3002/register/forgot",
-//       {}
-//     );
-//     console.log(response);
-//     // if (response.data) {
-//     //   navigate("/reset");
-//     // } else {
-//     //   console.log("error in forgot");
-//     // }
-//   };
-//   return (
-//     <>
-//       <form>
-//         <div>
-//           <TextField
-//             type="email"
-//             name="email"
-//             label="Email"
-//             variant="standard"
-//             // value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//         </div>
-//         <br />
-
-//         <br />
-//         <Button
-//           variant="contained"
-//           type="submit"
-//           onClick={(e) => {
-//             handlesubmit(e);
-//           }}
-//         >
-//           Submit
-//         </Button>
-//       </form>
-//     </>
-//   );
-// }
 export default function Forgot() {
   const navigate = useNavigate();
   const [email, setUsername] = useState("");
