261c4cbae2a76d7ec7410964be6efb2f987ee7d8
MDhanesh <mgdhanesh98@gmail.com>
2022-10-24 19:08:17 +0530

    update

--- src/component/login.js
+++ src/component/login.js
@@ -32,10 +32,10 @@ export default function LoginComponent() {
             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
               <div
                 className="card bg-dark text-white"
-                style="border-radius: 1rem;"
+                style={{ borderradius: "1rem" }}
               >
                 <div className="card-body p-5 text-center">
-                  <div className="mb-md-5 mt-md-4 pb-5">
+                  <div className=" mt-md-4 pb-5">
                     <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
                     <p className="text-white-50 mb-5">
                       Please enter your login and password!
@@ -57,7 +57,7 @@ export default function LoginComponent() {
                       </div>
 
                       <div className="form-outline form-white mb-4">
-                        <label className="form-label" for="typePasswordX">
+                        <label className="form-label" htmlFor="typePasswordX">
                           Password
                         </label>
                         <input
@@ -87,27 +87,14 @@ export default function LoginComponent() {
                         Login
                       </button>
                     </form>
-                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                      <a href="#!" className="text-white">
-                        <i className="fab fa-facebook-f fa-lg"></i>
-                      </a>
-                      <a href="#!" className="text-white">
-                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
-                      </a>
-                      <a href="#!" className="text-white">
-                        <i className="fab fa-google fa-lg"></i>
-                      </a>
-                    </div>
                   </div>
 
-                  <div>
-                    <p className="mb-0">
-                      Don't have an account?{" "}
-                      <Link to="/signup" className="text-white-50 fw-bold">
-                        Sign Up
-                      </Link>
-                    </p>
-                  </div>
+                  <p className="">
+                    Don't have an account?{" "}
+                    <Link to="/signup" className="text-white-50 fw-bold">
+                      Sign Up
+                    </Link>
+                  </p>
                 </div>
               </div>
             </div>
