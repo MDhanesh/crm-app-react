7d5da97c0afd618543dc8612c077311f0d7c83f0
MDhanesh <mgdhanesh98@gmail.com>
2022-10-25 08:15:49 +0530

    update

--- src/component/services.js
+++ src/component/services.js
@@ -1,95 +1,205 @@
-import React from "react";
-import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
+import { Typography, Button } from "@mui/material";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+//import NavBar from "./NavBar";
+// import DeleteProduct from "./DeleteProduct";
+// import { env } from "./Config";
 
-const AddServices = () => {
-  const [name, setName] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [type, setType] = useState("");
-  const [joined, setJoined] = useState("");
-  const [status, setStatus] = useState("");
-
+function AddServices() {
   const navigate = useNavigate();
 
+  const [formData, setFormData] = useState({
+    producturl: "",
+    productname: "",
+    price: "",
+    quantity: "",
+    type: "",
+  });
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post(
+        `https://crm-node-app.herokuapp.com/contact/create/`,
+        { products: { ...formData } },
+        { headers: { accesstoken: localStorage.getItem("token") } }
+      );
+      //   const notify = () =>
+      //     toast("Product Added Successfully", {
+      //       position: "top-right",
+      //       autoClose: 4000,
+      //     });
+      //   notify();
+      console.log(response);
+      setTimeout(() => {
+        navigate("/homepage");
+      }, 5000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlecancel = () => {
+    navigate("/homepage");
+  };
+
   return (
-    <div>
-      <div className="add-user">
-        <TextField
-          onChange={(event) => setName(event.target.value)}
-          label="Name"
-          variant="standard"
-        />
+    <>
+      {/* <NavBar /> */}
+      <div style={{ display: "flex" }}>
+        <div
+          className="product col-md-3 col-lg-4"
+          style={{
+            justifyContent: "center",
+            padding: "30px",
+            backgroundColor: "white",
+            paddingLeft: "62px",
+          }}
+        >
+          <Typography
+            variant="h4"
+            style={{ fontSize: "30px", color: "black", fontWeight: "bolder" }}
+          >
+            Add Product
+          </Typography>
 
-        <TextField
-          onChange={(event) => setMobile(event.target.value)}
-          label="Mobile"
-          variant="standard"
-        />
+          <form onSubmit={handleSubmit}>
+            <div>
+              <TextField
+                className="textfield"
+                sx={{
+                  width: 300,
+                }}
+                id="standard-basic"
+                label="ProductUrl"
+                required
+                variant="standard"
+                name="name"
+                value={formData.producturl}
+                onChange={(e) =>
+                  setFormData({ ...formData, producturl: e.target.value })
+                }
+              />
+            </div>
+            <br />
+            <div>
+              <TextField
+                className="textfield"
+                sx={{
+                  width: 300,
+                }}
+                id="standard-basic"
+                label="Product Name"
+                required
+                variant="standard"
+                name="productname"
+                value={formData.productname}
+                onChange={(e) =>
+                  setFormData({ ...formData, productname: e.target.value })
+                }
+              />
+            </div>
+            <br />
+            <div>
+              <TextField
+                className="textfield"
+                sx={{
+                  width: 300,
+                }}
+                id="standard-basic"
+                label="Product Price"
+                required
+                variant="standard"
+                type="text"
+                name="price"
+                value={formData.price}
+                onChange={(e) =>
+                  setFormData({ ...formData, price: e.target.value })
+                }
+              />
+            </div>
+            <br />
+            <div>
+              <TextField
+                className="textfield"
+                sx={{
+                  width: 300,
+                }}
+                id="standard-basic"
+                label="Quantity"
+                variant="standard"
+                required
+                type="number"
+                name="confirmpassword"
+                value={formData.quantity}
+                onChange={(e) =>
+                  setFormData({ ...formData, quantity: e.target.value })
+                }
+              />
+            </div>
+            <br />
+            <div>
+              <select
+                style={{ width: "300px" }}
+                class="form-select"
+                aria-label="Default select example"
+                value={formData.type}
+                onChange={(e) =>
+                  setFormData({ ...formData, type: e.target.value })
+                }
+              >
+                <option selected>Select Type</option>
+                <option value="camera">Camera</option>
+                <option value="lens">Lens</option>
+                <option value="light">Light</option>
+              </select>
+            </div>
+            <br />
+            <div>
+              <Button
+                style={{
+                  width: "120px",
+                  marginRight: "53px",
+                }}
+                variant="contained"
+                onClick={handlecancel}
+              >
+                Back
+              </Button>
+              <Button
+                style={{
+                  width: "120px",
+                }}
+                variant="contained"
+                type="submit"
+              >
+                Add
+              </Button>
+              {/* <ToastContainer /> */}
+            </div>
+          </form>
+        </div>
+        {/* <div className="col-lg-8" style={{ backgroundColor: "#99A3A4" }}>
+          <p
+            style={{
+              fontSize: "30px",
+              color: "black",
+              fontWeight: "bolder",
+              textAlign: "center",
 
-        <TextField
-          onChange={(event) => setEmail(event.target.value)}
-          label="Email"
-          variant="standard"
-        />
-
-        <TextField
-          onChange={(event) => setAddress(event.target.value)}
-          label="Address"
-          variant="standard"
-        />
-
-        <TextField
-          onChange={(event) => setType(event.target.value)}
-          label="Type"
-          variant="standard"
-        />
-
-        <TextField
-          onChange={(event) => setJoined(event.target.value)}
-          label="Joined Date"
-          variant="standard"
-        />
-
-        <TextField
-          onChange={(event) => setStatus(event.target.value)}
-          label="Status"
-          variant="standard"
-        />
+              backgroundColor: "skyblue",
+              padding: "15px",
+            }}
+          >
+            Delete Product
+          </p>
+          <DeleteProduct style={{ padding: "0px", margin: "0px" }} />
+        </div> */}
       </div>
-
-      <Button
-        className="add-button"
-        onClick={() => {
-          const newCustomer = {
-            name: name,
-            mobile: mobile,
-            email: email,
-            address: address,
-            type: type,
-            joined: joined,
-            status: status,
-          };
-
-          fetch("https://crm-node-app.herokuapp.com/services/create", {
-            method: "POST",
-            body: JSON.stringify(newCustomer),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-            .then((data) => data.json())
-            .then(() => navigate("/dashboard"));
-        }}
-        variant="contained"
-      >
-        Add Customer
-      </Button>
-    </div>
+    </>
   );
-};
-
+}
 export default AddServices;
